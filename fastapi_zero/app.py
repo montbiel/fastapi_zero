f56202b89950464d8ab2f0011a0409c7ef72237c
Gabriel Monteiro <gabrielmonteiro@Mac-mini.local>
2025-08-01 12:03:22 -0300

    implementando rotas CRUD

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,8 +1,59 @@
-from fastapi import FastAPI
+from http import HTTPStatus
+
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import HTMLResponse
+
+from fastapi_zero.schemas import UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI()
+database = []
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_class=HTMLResponse)
 def read_root():
-    return {'message': 'olá mundo'}
+    return """
+    <html>
+        <head>
+            <title>Olá mundo</title>
+        </head>
+        <body>
+            <h1>Olá mundo</h1>
+        </body>
+    </html>
+    """
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+    database.append(user_with_id)
+    return user_with_id
+
+
+@app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
+def read_users():
+    return {'users': database}
+
+
+@app.put(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
+)
+def update_user(user_id: int, user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database))
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='Não encontrei'
+        )
+    database[user_id - 1] = user_with_id
+    return user_with_id
+
+
+@app.delete(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
+)
+def delete_user(user_id: int):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='Não encontrei'
+        )
+    return database.pop(user_id - 1)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,16 +1,72 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fastapi_zero.app import app
-
-client = TestClient(app)
-
-
-def test_root_deve_retornar_ola_mundo():
-    client = TestClient(app)
-
+def test_root_deve_retornar_ola_mundo(client):
     response = client.get('/')
 
-    assert response.json() == {'message': 'olá mundo'}
+    assert (
+        response.text
+        == """
+    <html>
+        <head>
+            <title>Olá mundo</title>
+        </head>
+        <body>
+            <h1>Olá mundo</h1>
+        </body>
+    </html>
+    """
+    )
     assert response.status_code == HTTPStatus.OK
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'Alice',
+            'password': '123456',
+            'email': 'alice@example.com',
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'id': 1,
+        'username': 'Alice',
+        'email': 'alice@example.com',
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [{'id': 1, 'username': 'Alice', 'email': 'alice@example.com'}]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'bob',
+            'password': 'secret',
+            'email': 'bob@example.com',
+        },
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': 1,
+        'username': 'bob',
+        'email': 'bob@example.com',
+    }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'email': 'bob@example.com',
+        'username': 'bob',
+        'id': 1,
+    }
